892ae332e6d0052256e0c003787b5c486067caaa
sairam566 <sairam1431995@gmail.com>
2023-09-16 18:29:39 +0530

    DTO

---
+++ 6_MVC_Abstract_Controller/src/main/java/com/tsr/dto/AppointmentDto.java
@@ -0,0 +1,43 @@
+package com.tsr.dto;
+
+import java.time.LocalDateTime;
+
+public class AppointmentDto {
+	private int appointmentNo;
+	private String patientName;
+	private String doctorName;
+	private LocalDateTime appointmentDate;
+
+	public int getAppointmentNo() {
+		return appointmentNo;
+	}
+
+	public void setAppointmentNo(int appointmentNo) {
+		this.appointmentNo = appointmentNo;
+	}
+
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	public String getDoctorName() {
+		return doctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+
+	public LocalDateTime getAppointmentDate() {
+		return appointmentDate;
+	}
+
+	public void setAppointmentDate(LocalDateTime appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+}
