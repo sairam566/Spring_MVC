758a85659044ac47cc87be205b5cbb63e62dfa1a
sairam566 <sairam1431995@gmail.com>
2023-10-03 11:16:20 +0530

    AbstractAnnotationConfigDispatcherServletInitializer

---
+++ 13_MVC_Annotation/src/main/java/com/pro/regi/MyApplicationRegi.java
@@ -0,0 +1,25 @@
+package com.pro.regi;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.tsr.config.MVCConfig;
+import com.tsr.config.RootConfig;
+
+public class MyApplicationRegi extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {MVCConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"*.htm"};
+	}
+	
+}
