e417041e7776b395dd7ec74331d0dbc2f313419c
sairam566 <sairam1431995@gmail.com>
2023-10-12 10:03:39 +0530

    Register

---
+++ 15_MVC_Annotation_HandlerInterceptor/src/main/java/com/register/AnnotationConfig.java
@@ -0,0 +1,26 @@
+package com.register;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@EnableWebMvc
+public class AnnotationConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebMVCConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"*.htm"};
+	}
+
+}
