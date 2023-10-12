922dc71d4e0e938d616fe49e461e8fcb1e115bc2
sairam566 <sairam1431995@gmail.com>
2023-10-12 09:20:17 +0530

    Controller

---
+++ 15_MVC_Annotation_HandlerInterceptor/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello.htm")
+	public String hello() {
+		return "hello";
+	}
+}
