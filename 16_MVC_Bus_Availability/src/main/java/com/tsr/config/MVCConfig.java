fe4e286778d087936d4c85f1fb03929da62e4b3b
sairam566 <sairam1431995@gmail.com>
2023-10-17 10:25:50 +0530

    Config

---
+++ 16_MVC_Bus_Availability/src/main/java/com/tsr/config/MVCConfig.java
@@ -0,0 +1,20 @@
+package com.tsr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.tsr"})
+public class MVCConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/jsp/", ".jsp");
+	}
+
+	
+}
