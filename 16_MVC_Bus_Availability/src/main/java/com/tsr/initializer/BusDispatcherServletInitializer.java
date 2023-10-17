a4488bbaf588a93f223326b11f5b6b61beb14bec
sairam566 <sairam1431995@gmail.com>
2023-10-17 10:26:20 +0530

    DispatcherServlet

---
+++ 16_MVC_Bus_Availability/src/main/java/com/tsr/initializer/BusDispatcherServletInitializer.java
@@ -0,0 +1,25 @@
+package com.tsr.initializer;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.tsr.config.MVCConfig;
+import com.tsr.config.RootConfig;
+
+public class BusDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {MVCConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"*.htm"};
+	}
+	
+}
