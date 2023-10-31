cae5d0e687dac16279245adab358a23309e52af7
sairam566 <sairam1431995@gmail.com>
2023-10-31 11:28:36 +0530

    Validation based on groups

--- 21_Java_Custom_Validator/src/main/java/com/tsr/App.java
+++ 21_Java_Custom_Validator/src/main/java/com/tsr/App.java
@@ -3,11 +3,13 @@ package com.tsr;
 import java.util.Set;
 
 import com.tsr.beans.LoanApplication;
+import com.tsr.validator.AgricultureGroup;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import jakarta.validation.groups.Default;
 
 public class App 
 {
@@ -17,15 +19,18 @@ public class App
     			.loanApplicationNo(566)
     			.applicantName("Loan Manager")
     			.loanType("home")
-    			.tenure(24)
+    			.tenure(23)
     			.loanAmount(200000)
-    			.emailAddress("test@gmail.com")
-    			.contactNo("213265478").build();
+    			.emailAddress("tsr@gmailcom")
+    			.contactNo("213265478")
+    			.cropType("Wheet")
+    			.acres(1)
+    			.build();
     	
     	ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
 		Validator validator = validatorFactory.getValidator();
 		
-		Set<ConstraintViolation<LoanApplication>> constraintViolations = validator.validate(application);
+		Set<ConstraintViolation<LoanApplication>> constraintViolations = validator.validate(application,AgricultureGroup.class,Default.class);
 		for(ConstraintViolation<LoanApplication> violation : constraintViolations) {
     		System.out.println(violation.getMessage());
     	}
