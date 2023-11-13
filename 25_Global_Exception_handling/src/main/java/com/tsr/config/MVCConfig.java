48cf74ad648e4d07021d25d01d34ea6aaed50643
sairam566 <sairam1431995@gmail.com>
2023-11-13 22:43:09 +0530

    Config

---
+++ 25_Global_Exception_handling/src/main/java/com/tsr/config/MVCConfig.java
@@ -0,0 +1,29 @@
+package com.tsr.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.tsr.controller"})
+public class MVCConfig implements WebMvcConfigurer{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/jsp/", ".jsp");
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.addBasenames("errors");
+		return messageSource;
+	}
+	
+}
