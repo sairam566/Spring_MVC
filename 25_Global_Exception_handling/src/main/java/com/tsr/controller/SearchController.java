b661e87d21fdadab9ac026f7c5081df183a3f360
sairam566 <sairam1431995@gmail.com>
2023-11-13 22:42:31 +0530

    Controller

---
+++ 25_Global_Exception_handling/src/main/java/com/tsr/controller/SearchController.java
@@ -0,0 +1,49 @@
+package com.tsr.controller;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tsr.exceptions.ProductNotFoundException;
+import com.tsr.form.SearchForm;
+import com.tsr.service.SearchService;
+
+@Controller
+@RequestMapping("/search.htm")
+public class SearchController {
+	
+	@Autowired
+	private SearchService searchService;
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@GetMapping
+	public String displaySearch(ModelMap modelMap) {
+		SearchForm form = new SearchForm();
+		modelMap.addAttribute("itemForm", form);
+		return "search-product";
+	}
+	
+	@PostMapping
+	public String searchProduct(@ModelAttribute("itemForm") SearchForm form, BindingResult errors,ModelMap modelMap) {
+		
+		if(errors.hasErrors()) {
+			return "search-product";
+		}
+		String product = searchService.getSearchProduct(form);
+		modelMap.addAttribute("productName", product);
+		return "display-product";
+	}
+	
+}
