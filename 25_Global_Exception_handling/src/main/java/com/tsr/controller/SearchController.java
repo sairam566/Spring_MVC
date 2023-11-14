69d4890d55bdaac3278a6dbda62036638fe3b0cc
sairam566 <sairam1431995@gmail.com>
2023-11-14 09:57:44 +0530

    removed unnecessary imports

--- 25_Global_Exception_handling/src/main/java/com/tsr/controller/SearchController.java
+++ 25_Global_Exception_handling/src/main/java/com/tsr/controller/SearchController.java
@@ -1,20 +1,15 @@
 package com.tsr.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.tsr.exceptions.ProductNotFoundException;
 import com.tsr.form.SearchForm;
 import com.tsr.service.SearchService;
 
