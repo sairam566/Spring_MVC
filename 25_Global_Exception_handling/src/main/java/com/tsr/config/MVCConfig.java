28e156dff3bccc6506f2af06edccb7d9e1d14279
sairam566 <sairam1431995@gmail.com>
2023-11-14 10:49:05 +0530

    HandlerExceptionResolver

--- 25_Global_Exception_handling/src/main/java/com/tsr/config/MVCConfig.java
+++ 25_Global_Exception_handling/src/main/java/com/tsr/config/MVCConfig.java
@@ -1,13 +1,19 @@
 package com.tsr.config;
 
+import java.util.Properties;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import com.tsr.exceptions.InvalidProductException;
 
 @Configuration
 @EnableWebMvc
@@ -24,6 +30,15 @@ public class MVCConfig implements WebMvcConfigurer{
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.addBasenames("errors");
 		return messageSource;
+	}
+	
+	@Bean
+	public HandlerExceptionResolver genericExceptions() {
+		SimpleMappingExceptionResolver resolver = new SimpleMappingExceptionResolver();
+		Properties properties = new Properties();
+		properties.put(InvalidProductException.class.getName(), "global-error");
+		resolver.setExceptionMappings(properties);
+		return resolver;
 	}
 	
 }
